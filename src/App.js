17072733a0ac28c12e5dc512537e29674b896db1
huzaifa aslam <huzaifaslam45@gmail.com>
2020-12-04 17:52:08 +0500

    sf

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Counter from './components/Counter'
+import HelloWorld from '@bit/huzaifa-aslam.helloworld.hello-world';
 
 function App() {
   return (
     <div className="App">
+      <HelloWorld/>
       <Counter/>
     </div>
   );
